6c84b349137d8d89cc9e6b63350c9a49ecf98cef
Wilson Wong <wilsonwong2988@gmail.com>
2018-01-04 00:18:39 -0800

    Finish site
    So much for spacing out the commits, finished it in a couple hours. Some refactoring is in order, though. The scrollhandler.js could also be converted to use jQuery.

--- js/scrollhandler.js
+++ js/scrollhandler.js
@@ -9,19 +9,40 @@ var sticky = navbar.offsetTop;
 function stickNavbar() {
     if (window.pageYOffset >= sticky) {
         navbar.classList.add("sticky");
-        container.style.paddingTop = String(container.style.paddingTop + navbar.offsetHeight + "px");
     } else {
         navbar.classList.remove("sticky");
-        container.style.removeProperty('padding-top');
-        if (container.getAttribute('style') === "") {
-            container.removeAttribute('style');
-        }
     }
 }
 
 /*=== Smooth scroll functionality ===*/
 
+// We're only scrolling to the sections defined by the nav. If you wish to customize this,
+// you'll have to select the elements and attach event listeners yourself.
+var navLinks = document.getElementsByClassName("nav-link");
 
+for (var i = 0; i < navLinks.length; i++) {
+    var targetName = navLinks[i].getAttribute('href');
+    var target = document.querySelector(targetName);
+
+    // We need a closure to keep the right value of target by the time the function runs
+    var targetClosure = function(target) {
+        return function(ev) { scrollTo(ev, target); };
+    };
+
+    var handler = targetClosure(target);
+
+    navLinks[i].addEventListener('click', handler, false);
+}
+
+function scrollTo(ev, elem) {
+    ev.preventDefault();
+    elem.focus();
+    elem.scrollIntoView({
+        behavior: 'smooth'
+    })
+}
+
+// Not used. Can be useful for scrolling to any arbritrary element if you wish to do that.
 function getElemDistance(elem) {
     var location = 0;
     if (elem.offsetParent) {
